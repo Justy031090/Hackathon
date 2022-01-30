dc1f3a5f8bdf9783846d64d68eece06b2a62bd74
IlyaMeli <ilyameli93@gmail.com>
2022-01-30 20:38:04 +0200

    added iframe + client to server bridge

--- client/src/pages/SongPage/SongPage.jsx
+++ client/src/pages/SongPage/SongPage.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./SongPage.css";
-const SongPage = ({ song }) => {
+const SongPage = ({ song, songData, videoId }) => {
   return (
-    <div className="song-page">
-      <button className="back-btn">
-        <Link to="/search">Back</Link>
-      </button>
-      <header className="poster">
-        <div className="img-placeholder">Poster</div>
-        <div className="info-container">
-          <p>{song.name}</p>
-          <div className="genre-container">
-            <p>{song.artist}</p>
-            <p>{song.genre}</p>
-          </div>
-        </div>
-      </header>
-      <div className="main-content">
-        <div className="video-container">VIDEO</div>
-        <div className="lyrics-container">
-          <h2>Lyrics</h2>
-          <p>
+    <>
+      {songData && (
+        <div className="song-page">
+          <button className="back-btn">
+            <Link to="/search">Back</Link>
+          </button>
+          <header className="poster">
+            <img src={songData.poster} className="img-placeholder" />
+            <div className="info-container">
+              <p>{songData.song}</p>
+              <div className="genre-container">
+                <p>{songData.artist}</p>
+                <p>{songData.genre}</p>
+              </div>
+            </div>
+          </header>
+          <div className="main-content">
+            {/* <div className="video-container">VIDEO</div> */}
+            <iframe
+              src={`http://www.youtube.com/embed/${videoId}`}
+              className="video-container"
+            ></iframe>
+            <div className="lyrics-container">
+              <h2>Lyrics</h2>
+              {/* <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
             qui accusantium ipsam, aut nulla reiciendis eos distinctio
             repellendus, ratione expedita consectetur esse neque perferendis,
@@ -34,10 +40,13 @@ const SongPage = ({ song }) => {
             expedita nobis commodi adipisci quo at. Ullam nostrum ad minus
             praesentium. Veritatis eveniet animi labore suscipit eos officiis
             tempore ex. Delectus reprehenderit ut eveniet quisquam?
-          </p>
+          </p> */}
+              <p>{songData.lyrics}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 export default SongPage;
