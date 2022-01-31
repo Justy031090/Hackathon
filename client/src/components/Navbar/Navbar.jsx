4f9db13b76e7cb808eda2450377504a625e209c8
justy031090 <justy031090@gmail.com>
2022-01-31 10:18:16 +0200

    localhost link deleted

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,46 +1,45 @@
-import React, { useState } from "react";
-import Dropdown from "../Dropdown/Dropdown";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
-import axios from "axios";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import axios from 'axios';
 
 const Navbar = ({ setSongData, songData, setVideoId }) => {
-  const [inputValue, setInputValue] = useState("");
+    const [inputValue, setInputValue] = useState('');
 
-  const handleSearch = async () => {
-    const { data } = await axios.post("http://localhost:5000/api/songs", {
-      search: inputValue,
-    });
-    console.log(data);
-    setSongData(data);
-    console.log(process.env);
-  };
-  const handleYoutube = async () => {
-    const { data } = await axios.get(
-      `https://youtube.googleapis.com/youtube/v3/search?maxResults=1&q=${inputValue}&key=AIzaSyDWITkJ-1nt_mBdgC55w4uDmdRfvmKdkFA`
-    );
-    const videoId = data.items[0].id.videoId;
-    setVideoId(videoId);
-  };
-  return (
-    <nav>
-      <Link to="/">Home</Link>
-      <input
-        type="text"
-        placeholder="Input your keywords here...."
-        onChange={(e) => {
-          setInputValue(e.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          handleSearch();
-          handleYoutube();
-        }}
-      >
-        <Link to="/song">Search</Link>
-      </button>
-      {/* <Dropdown defaultOption="Select a Category">
+    const handleSearch = async () => {
+        const { data } = await axios.post('/api/songs', {
+            search: inputValue,
+        });
+        console.log(data);
+        setSongData(data);
+        console.log(process.env);
+    };
+    const handleYoutube = async () => {
+        const { data } = await axios.get(
+            `https://youtube.googleapis.com/youtube/v3/search?maxResults=1&q=${inputValue}&key=AIzaSyDWITkJ-1nt_mBdgC55w4uDmdRfvmKdkFA`
+        );
+        const videoId = data.items[0].id.videoId;
+        setVideoId(videoId);
+    };
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <input
+                type="text"
+                placeholder="Input your keywords here...."
+                onChange={(e) => {
+                    setInputValue(e.target.value);
+                }}
+            />
+            <button
+                onClick={() => {
+                    handleSearch();
+                    handleYoutube();
+                }}
+            >
+                <Link to="/song">Search</Link>
+            </button>
+            {/* <Dropdown defaultOption="Select a Category">
         <option>Pop</option>
         <option>Rock</option>
         <option>EDM</option>
@@ -50,7 +49,7 @@ const Navbar = ({ setSongData, songData, setVideoId }) => {
         <option>English</option>
         <option>Arabic</option>
       </Dropdown> */}
-    </nav>
-  );
+        </nav>
+    );
 };
 export default Navbar;
