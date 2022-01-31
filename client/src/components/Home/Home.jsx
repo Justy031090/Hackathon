33f531e68b11bd21de57338d6ffe7e4dcf2ff3cb
IlyaMeli <ilyameli93@gmail.com>
2022-01-31 11:48:50 +0200

    fixing endter on keypress

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,17 +3,17 @@ import TextTransition, { presets } from "react-text-transition";
 import "../Home/Home.css";
 
 const TEXTS = [
-  `Welcome to the PlaceHolder, the site was built in order to solve the
+  `Welcome to the ShirZam, the site was built in order to solve the
 language barrier that exists communities we have in our country, so
 feel free to search for any song you like and translate it into your
 own language`,
   `          مرحبا الى الاسم , هذا الموقع بني من اجل ان يحل مشكلة اللغة التي موجودة
   في دولتنا , اشعر بالحرية ببحث عن اغنية وترجمتها الى لغتك`,
   `
-  Добро пожаловать в PlaceHolder, сайт был создан для того что бы преодолеть языковой барьер,
+  Добро пожаловать в ShirZam, сайт был создан для того что бы преодолеть языковой барьер,
    который существует в сообществах, которые есть в нашей стране, поэтому не стесняйтесь искать
     любую понравившуюся песню и переводить её на свой язык`,
-  `ברוכים הבאים לפלייסהולדר , האתר נבנה לטובת 
+  `ברוכים הבאים לשירזם , האתר נבנה לטובת 
   לפתור את מחסום השפה שיש בקהילות הרבות שיש לנו במדינה,
    אז תרגישו חופשיים לחפש שיר שאתם אוהבים ותתרגמו אותו לשפה שנוחה רק לכם `,
 ];
@@ -23,10 +23,7 @@ const Home = () => {
   let textStyle = {};
 
   useEffect(() => {
-    const intervalId = setInterval(
-      () => setIndex((index) => index + 1),
-      5000 // every 3 seconds
-    );
+    const intervalId = setInterval(() => setIndex((index) => index + 1), 5000);
     return () => clearTimeout(intervalId);
   }, []);
 
@@ -41,7 +38,9 @@ const Home = () => {
 
   return (
     <div className="home-container">
-      <h1 className="home-title">PlaceHolder </h1>
+      <h1 className="home-title">
+        <span className="home-title2">Shir</span>Zam
+      </h1>
       <div className="home-content" style={textStyle}>
         <TextTransition
           text={TEXTS[index % TEXTS.length]}
