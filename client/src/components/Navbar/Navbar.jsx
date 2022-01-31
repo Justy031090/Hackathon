ecb58c86e6fcf1bd8881ca384ac9607031ceb56d
justy031090 <justy031090@gmail.com>
2022-01-31 12:16:57 +0200

    youtube api exceeded..new added

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,62 +1,63 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
-import axios from "axios";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import axios from 'axios';
 
 const Navbar = ({ setSongData, songData, setVideoId }) => {
-  const [inputValue, setInputValue] = useState("");
+    const [inputValue, setInputValue] = useState('');
 
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  const handleSearch = async () => {
-    let url = "http://localhost:5000/api/songs";
-    if (process.env.NODE_ENV === "production") {
-      url = "/api/songs";
-    }
-    const { data } = await axios.post(url, {
-      search: inputValue,
-      config,
-    });
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    const handleSearch = async () => {
+        let url = 'http://localhost:5000/api/songs';
+        if (process.env.NODE_ENV === 'production') {
+            url = '/api/songs';
+        }
+        const { data } = await axios.post(url, {
+            search: inputValue,
+            config,
+        });
 
-    setSongData(data);
-  };
-  const handleYoutube = async () => {
-    const { data } = await axios.get(
-      `https://youtube.googleapis.com/youtube/v3/search?maxResults=1&q=${inputValue}&key=AIzaSyDWITkJ-1nt_mBdgC55w4uDmdRfvmKdkFA`
+        setSongData(data);
+    };
+    const handleYoutube = async () => {
+        const { data } = await axios.get(
+            `https://youtube.googleapis.com/youtube/v3/search?maxResults=1&q=${inputValue}&key=${process.env.YOUTUBE_API}
+            `
+        );
+        const videoId = data.items[0].id.videoId;
+        setVideoId(videoId);
+    };
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <input
+                className="search-input"
+                type="text"
+                placeholder="Input your keywords here...."
+                onChange={(e) => {
+                    setInputValue(e.target.value);
+                }}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') handleSearch();
+                    handleYoutube();
+                }}
+            />
+            <Link to="/song">
+                <button
+                    className="search-btn"
+                    onClick={() => {
+                        handleSearch();
+                        handleYoutube();
+                    }}
+                >
+                    Search
+                </button>
+            </Link>
+        </nav>
     );
-    const videoId = data.items[0].id.videoId;
-    setVideoId(videoId);
-  };
-  return (
-    <nav>
-      <Link to="/">Home</Link>
-      <input
-        className="search-input"
-        type="text"
-        placeholder="Input your keywords here...."
-        onChange={(e) => {
-          setInputValue(e.target.value);
-        }}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") handleSearch();
-          handleYoutube();
-        }}
-      />
-      <Link to="/song">
-        <button
-          className="search-btn"
-          onClick={() => {
-            handleSearch();
-            handleYoutube();
-          }}
-        >
-          Search
-        </button>
-      </Link>
-    </nav>
-  );
 };
 export default Navbar;
